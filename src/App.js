a4d6c12967366fd942f6af0bee9aa7ec574b7716
Xarizmo <mpolikarpoff@gmail.com>
2020-09-22 14:21:10 +0300

    Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Let's build the facebook clone!</h1>
+      <Header />
     </div>
   );
 }
